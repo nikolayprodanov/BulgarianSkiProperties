3dad3cded56c12598ee2cfc380eac963214fa6fa
Nikolay Prodanow <nikolayprodanow@gmail.com>
2017-12-10 19:14:13 +0200

    Added swinject setup file

---
+++ BulgarianSkiProperties/Utils/Swinject/SwinjectSetup.swift
@@ -0,0 +1,23 @@
+//
+//  SwinjectSetup.swift
+//  BulgarianSkiProperties
+//
+//  Created by Nikolay Prodanow on 10.12.17.
+//  Copyright © 2017 np. All rights reserved.
+//
+
+import Foundation
+import Swinject
+
+class SwinjectSetup {
+    /// Starts Swinject setuping
+    static func start() -> Container {
+        let container = Container()
+     
+        // Variables
+        
+        // VC
+        
+        return container
+    }
+}
