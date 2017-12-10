8c6706b257538e735715f9419a36ea5624d68a42
Nikolay Prodanow <nikolayprodanow@gmail.com>
2017-12-10 18:54:52 +0200

    Added Utils folder and some basic implementation for Hanling texts

---
+++ BulgarianSkiProperties/Utils/Localizer/Localizer.swift
@@ -0,0 +1,15 @@
+//
+//  Localizer.swift
+//  BulgarianSkiProperties
+//
+//  Created by Nikolay Prodanow on 10.12.17.
+//  Copyright © 2017 np. All rights reserved.
+//
+
+import Foundation
+
+struct Localizer {
+    static func localize(key: String) -> String {
+        return Texts.mainScreenTitle
+    }
+}
---
+++ BulgarianSkiProperties/Utils/Localizer/Texts.swift
@@ -0,0 +1,13 @@
+//
+//  Texts.swift
+//  BulgarianSkiProperties
+//
+//  Created by Nikolay Prodanow on 10.12.17.
+//  Copyright © 2017 np. All rights reserved.
+//
+
+import Foundation
+
+struct Texts {
+    static let mainScreenTitle = "Bulgarian Ski Properties"
+}
